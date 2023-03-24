b6927f6d478c5ddba18a3514c2622eb3ee406826
Heithem KACEM <heithem.kacem@gmail.com>
2023-03-24 14:33:09 +0100

    Add : auth services (login and signup) and books endpoind to test

---
+++ src/auth/dto/signup.dto.ts
@@ -0,0 +1,24 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class SignUpDto {
+  @IsNotEmpty()
+  @IsString()
+  readonly fullName: string;
+  @IsNotEmpty()
+  @IsNumber()
+  readonly phone: string;
+  @IsNotEmpty()
+  @IsEmail({}, { message: 'Please enter correct email' })
+  readonly email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(6)
+  readonly password: string;
+}
